986a30604c3abef0737009639dcd92bce315f00d
Varshini2306 <118874235+Varshini2306@users.noreply.github.com>
2024-09-19 23:26:56 +0530

    Add files via upload

---
+++ src/AppTest.java
@@ -0,0 +1,23 @@
+package com.testqaci.qa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test; 
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+    	Assert.assertEquals("hello", "hello");
+    }
+}
---
+++ src/LoginTest.java
@@ -0,0 +1,42 @@
+package com.testqaci.qa;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
+
+import com.testqaci.qa.BaseClass;
+import com.testqaci.qa.LoginPage;
+
+public class LoginTest extends BaseClass {
+	private static Logger Logger = LogManager.getLogger(LoginTest.class);
+
+//	public void browserlaunch() throws InterruptedException {
+//
+//		BaseClass.openbrowser();
+//		Thread.sleep(5000);
+//		extent.flush();
+//
+//	}
+
+	@Test
+	public void loginprocess() throws InterruptedException {
+		BaseClass.openbrowser();
+		Thread.sleep(5000);
+		extent.flush();
+		LoginPage LP = new LoginPage(BaseClass.driver);
+		Logger.info("Add  credentials");
+
+		LP.username();
+		LP.Password();
+		LP.LoginBTW();
+
+		Logger.info("Login done now ");
+		Thread.sleep(2000);
+		extent.flush();
+	}
+
+}
